969d66010e99e869f79e9d6e645c0b04b2fbae0d
Michelsaravia <taniasaravia99@gmail.com>
2019-11-12 19:48:05 -0600

    agregamos (Volley) para guardado asi como se trabajó en la interfaz y clase para registrar alabanzas

--- app/src/main/java/com/example/proyecto/RegistroAlabanzas.java
+++ app/src/main/java/com/example/proyecto/RegistroAlabanzas.java
@@ -3,12 +3,31 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegistroAlabanzas extends AppCompatActivity {
+public class RegistroAlabanzas extends AppCompatActivity implements View.OnClickListener {
+
+     EditText etnombre, etautor, etletra;
+     Button btnR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_alabanzas);
+
+        etnombre = findViewById(R.id.R_Alabanzas);
+        etautor = findViewById(R.id.R_NAutor);
+        etletra = findViewById(R.id.R_LetraA);
+
+        btnR = findViewById(R.id.btn_reg);
+
+        btnR.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
