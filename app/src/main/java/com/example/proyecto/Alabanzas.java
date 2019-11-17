940b1fa83a5ea16453fba71a53bf935c3919b748
Glendaa <glenda.chanta567@gmail.com>
2019-11-17 11:06:03 -0600

    Contructor con todos los datos

--- app/src/main/java/com/example/proyecto/Alabanzas.java
+++ app/src/main/java/com/example/proyecto/Alabanzas.java
@@ -8,4 +8,11 @@ public class Alabanzas {
 
     public Alabanzas() {
     }
+
+    public Alabanzas(int id, String titulo, String autor, String letra) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.letra = letra;
+    }
 }
