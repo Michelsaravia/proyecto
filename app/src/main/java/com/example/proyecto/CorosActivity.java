4c380fb1a47d774834e73734aaad15c4d57ecac3
Michelsaravia <taniasaravia99@gmail.com>
2019-11-17 16:56:08 -0600

    se agrego metodo de agregarCoros

--- app/src/main/java/com/example/proyecto/CorosActivity.java
+++ app/src/main/java/com/example/proyecto/CorosActivity.java
@@ -10,8 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class CorosActivity extends AppCompatActivity {
 
@@ -61,7 +65,7 @@ public class CorosActivity extends AppCompatActivity {
     }
 
 
-    private void almacenarAlabanzas() {
+    private void almacenarCoros() {
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,6 +85,28 @@ public class CorosActivity extends AppCompatActivity {
 
                     //obtenerAlabanzas();
                 }
+            }
+        });
+    }
+
+
+    private  void agregarCoros(Coros a){
+        String url = "https://proyectofinalsis21.000webhostapp.com/agregarcoro.php?";
+        String parametros = "titulo="+a.getTitulo()+"&autor="+a.getAutor()+"&letra="+a.getLetra();
+        cliente.post(url + parametros, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200){
+                    Toast.makeText(CorosActivity.this, "Alabanza agregada correctamente", Toast.LENGTH_SHORT).show();
+                    ettitulo.setText("");
+                    etautor.setText("");
+                    etletra.setText("");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
             }
         });
     }
