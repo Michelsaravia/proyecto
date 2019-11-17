6da9fcd38b7f2600bdf549e777ae50ceec2ba363
Glendaa <glenda.chanta567@gmail.com>
2019-11-17 11:36:34 -0600

    Metodp almacenaralabanzas

--- app/src/main/java/com/example/proyecto/AlabanzasActivity.java
+++ app/src/main/java/com/example/proyecto/AlabanzasActivity.java
@@ -3,6 +3,7 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,5 +26,33 @@ public class AlabanzasActivity extends AppCompatActivity {
 
         cliente = new AsyncHttpClient();
 
+    }
+
+    private void almacenarAlabanzas() {
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ettitulo.getText().toString().length()== 0 )  {
+                    ettitulo.setError("Campo Obligatorio");
+                }else if (etautor.getText().toString().length()== 0){
+                    etautor.setError("Campo Obligatorio");
+                }else  if (etletra.getText().toString().length()== 0){
+                    etletra.setError("Campo Obligatorio");
+                }else{
+                    Alabanzas a = new Alabanzas();
+                    a.setTitulo(ettitulo.getText().toString().replaceAll(" ", "%20"));
+                    a.setAutor(etautor.getText().toString().replaceAll(" ", "%20"));
+                    a.setLetra(etletra.getText().toString().replaceAll(" ", "%20"));
+
+                    //agregarAlabanza(a);
+                    // try {
+                        //    Thread.sleep(1000);
+                        //} catch (InterruptedException e) {
+                        //    e.printStackTrace();
+                        //}
+                    // obtenerAlabanzas();
+                }
+            }
+        });
     }
 }
