115effc9c9783eb42a6174429085d77bb6f0b125
Glendaa <glenda.chanta567@gmail.com>
2019-11-17 12:36:37 -0600

    declaracion de variable para listview y objeto para AsyncHttpClient

--- app/src/main/java/com/example/proyecto/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto/registro_alabanzas.java
@@ -3,9 +3,13 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class registro_alabanzas extends AppCompatActivity {
-
+    private ListView lvdatos;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
