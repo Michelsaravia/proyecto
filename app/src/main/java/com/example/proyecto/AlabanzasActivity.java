01dcbb6c211ca752b1c71a73a6c68db3b75b3407
Glendaa <glenda.chanta567@gmail.com>
2019-11-17 11:19:24 -0600

    declaracion de variables para envio de datos

--- app/src/main/java/com/example/proyecto/AlabanzasActivity.java
+++ app/src/main/java/com/example/proyecto/AlabanzasActivity.java
@@ -3,9 +3,12 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AlabanzasActivity extends AppCompatActivity {
-
+    private EditText ettitulo, etautor, etletra;
+    private Button btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
