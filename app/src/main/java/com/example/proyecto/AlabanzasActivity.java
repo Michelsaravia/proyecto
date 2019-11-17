562fd9a8a7f6a3fcfb474b2718dbe809dd831788
Glendaa <glenda.chanta567@gmail.com>
2019-11-17 11:21:19 -0600

    declaracion del AsyncHttpClient

--- app/src/main/java/com/example/proyecto/AlabanzasActivity.java
+++ app/src/main/java/com/example/proyecto/AlabanzasActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.loopj.android.http.AsyncHttpClient;
+
 public class AlabanzasActivity extends AppCompatActivity {
     private EditText ettitulo, etautor, etletra;
     private Button btnRegistrar;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
