908b8ec3e7c5d85be1443cde5ff5f71f3d0e1b3f
Michelsaravia <taniasaravia99@gmail.com>
2019-11-17 16:17:36 -0600

    alertdialog en activity de registro

--- app/src/main/java/com/example/proyecto/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto/registro_alabanzas.java
@@ -3,7 +3,9 @@ package com.example.proyecto;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +24,34 @@ import cz.msebera.android.httpclient.Header;
 public class registro_alabanzas extends AppCompatActivity {
     private ListView lvdatos;
     private AsyncHttpClient cliente = new AsyncHttpClient();
+
+
+
+    AlertDialog.Builder dialogo;
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setIcon(R.drawable.ic_close)
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
